548b2f8b48ac1f64ee2e4a08c727e6fb1263c369
John Caraballo <jcaraballo019@gmail.com>
2022-03-14 17:12:05 -0400

    estimate the gas

--- main.py
+++ main.py
@@ -29,9 +29,11 @@ def post_trades(positionString):
   covey_ledger = w3.eth.contract(address = COVEY_LEDGER_ADDRESS, abi = ledger_info['abi'])
   nonce = w3.eth.get_transaction_count(WALLET)
 
+  gas = covey_ledger.functions.createContent(positionString).estimateGas({'from': WALLET, 'nonce': nonce})
+
   txn = covey_ledger.functions.createContent(positionString).buildTransaction({
     'chainId': int(POLYGON_CHAIN_ID),
-    'gas': 15000000, # can you estimate gas? 
+    'gas': gas,
     'nonce': nonce,
   })
   signed_txn = w3.eth.account.sign_transaction(txn, private_key=WALLET_PRIVATE_KEY)
@@ -45,5 +47,5 @@ def view_trades(address):
   # output format ('address', 'position string', unix time),
   print(result)
 
-#post_trades('FB:0.2,FNF:0.2,BTCUSDT:0.2,FNV:0.2,PLTR:0.2,GPS:0.2')
+post_trades('FB:0.2,FNF:0.2,BTCUSDT:0.2,FNV:0.2,PLTR:0.2,GPS:0.2')
 #view_trades(WALLET)
