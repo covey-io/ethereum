df7ae5f019d534eb2c7ce7984d6e5896d434336d
Brooker <52008580+brookerbjb@users.noreply.github.com>
2022-04-20 16:39:54 -0400

    Added prices csv instead of api call

--- portfolio.py
+++ portfolio.py
@@ -19,7 +19,6 @@ COVEY_LEDGER_POLYGON_ADDRESS = os.getenv('COVEY_LEDGER_POLYGON_ADDRESS')
 COVEY_LEDGER_SKALE_ADDRESS = os.getenv('COVEY_LEDGER_SKALE_ADDRESS')
 SKALE_URL = os.getenv('SKALE_URL')
 
-IEX_TOKEN = os.getenv('IEX_TOKEN')
 
 # Opening JSON file
 f = open('CoveyLedger.json')
@@ -47,24 +46,6 @@ def view_trades_polygon(address):
   # output format [('address', 'position string', unix time),('address', 'position string', unix time),...]
     print(result)
 
-
-def get_prices(symbols,exactDate):   
-    #docs:  '''https://iexcloud.io/docs/api/#historical-prices'''
-    root_url = 'https://cloud.iexapis.com/stable/stock/market/batch?symbols='
-    method = '&types=intraday-prices&'
-    exactDate = pd.to_datetime(exactDate,infer_datetime_format=True)
-    exactDate = exactDate.strftime('%Y%m%d')
-    quotes = dict()
-    for xOf100 in range(0,len(symbols),100) : 
-        urlVersion = symbols[xOf100:xOf100+100]
-        urlVersion = ",".join(urlVersion)
-        url = root_url+urlVersion+method+'&token='+IEX_TOKEN+'&exactDate='+exactDate
-        data = json.loads(requests.get(url).text)
-        quotes.update(data)
-    print(quotes)
-    return quotes 
-
-
 def calculate_portfolio(address,startCash):
 
     trades = view_trades_skale(address)
@@ -72,13 +53,11 @@ def calculate_portfolio(address,startCash):
     tradingKey = pd.DataFrame(columns=["id", "entry_price", "entry_date", "market_entry_date", "symbol", "percent", "current_position", 
     "adjusted_entry", "prior_portfolio_value", "target_position_value", "prior_position_value", "cash_used", 
     "share_count", "prior_cumulative_share_count", "post_cumulative_share_count", "status", "posted_on_chain", 
-    "address", "realized_profit", "exchange", "delayed_trade_date", "currency"]
+    "address", "realized_profit", "exchange", "delayed_trade_date", "currency"])
     
-    ''' FILL IN tradingKey here from trades'''
+    '''FILL IN tradingKey here from trades'''
 
-    prices = pd.DataFrame(columns=["id", "date_time", "price", "symbol"]
-
-    ''' FILL in prices here from tradingKey '''
+    prices = pd.read_csv('prices.csv')
 
     portfolio = pd.DataFrame(columns=["date_time", "address", "cash", "usd_value", 
                                       "inception_return"])
@@ -88,11 +67,12 @@ def calculate_portfolio(address,startCash):
     
     ''' CALCULATE PORTFOLIO HERE from tradingKey and prices '''
     
-    return tradkingKey, prices, portfolio
+    return tradingKey, prices, portfolio
 
 # Address One 
 #view_trades_skale('0x41da2035ac26e4308b624a84d3caebf80a4dccf1') 
 # Address Two 
 #view_trades_skale('0x211fe601e24ce89cb443356f687c67fbf7708412')
 # Price Pull
-#get_prices(['FB','GOOGL'],'2022-03-29')
+#print(pd.read_csv('prices.csv'))
+
